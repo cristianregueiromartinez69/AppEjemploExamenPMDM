eaf53eaa648b057bff56079052f8596b73e704c2
cristian <cregueiromartinez@danielcastelao.org>
2024-11-13 16:28:34 +0100

    viewmodel Integrado, vamos a UI

--- app/src/main/java/com/example/appexamen/model/Datos.kt
+++ app/src/main/java/com/example/appexamen/model/Datos.kt
@@ -1,17 +1,32 @@
 package com.example.appexamen.model
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Clase datos con los datos de la aplicacion
+ */
 object Datos {
 
-    var rondas = 0;
-    var aciertos = 0;
-    var record = 0;
+    /**
+     * Aqui tenemos los singletons de:
+     * 1. las rondas del juego
+     * 2. el record el jugador
+     * 3. los aciertos del jugador
+     * 4. la lista de la maquina que se va generando en cada ronda
+     * 5. la lista del jugador al pulsar botones
+     */
+    var rondas = mutableStateOf(0);
+    var aciertos = mutableStateOf(0);
+    var record = mutableStateOf(0);
     var secuenciaJuego = mutableListOf<Int>()
-    var sencuenciaJugador = mutableListOf<Int>()
+    var secuenciaJugador = mutableListOf<Int>()
 
 }
 
+/**
+ * Clase enum con los numeros, colores y colores iluminados
+ */
 enum class Colores(val numColor:Int, val colorInicial: Color, val colorIluminado:Color){
     ROJO(1, Color(0xFF9A0000), Color(0xFFFF0000)),
     VERDE(2, Color(0xFF00FF2A), Color(0xff006A11)),
