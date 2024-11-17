899ea95ea2b3df75fad89695b49d95959a0e8579
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 11:53:29 +0100

    creando nombre del jugador

--- app/src/main/java/com/example/appexamen/view/UIMain.kt
+++ app/src/main/java/com/example/appexamen/view/UIMain.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.appexamen.R
 import com.example.appexamen.viewmodel.ViewModel
 
@@ -29,14 +32,26 @@ fun Game(viewModel: ViewModel) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-        Column() {
-
+        Column {
+            NombreJUgador(viewModel)
         }
+
     }
 }
 
 @Composable
 fun NombreJUgador(viewModel: ViewModel){
 
+    Column( verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(top = 30.dp, start = 30.dp)) {
+
+        Text(
+            text = "Jugador: " + viewModel.getNombre(),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
 
