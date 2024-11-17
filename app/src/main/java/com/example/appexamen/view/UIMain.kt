f47346a14825f979959662f16e564ebb07d0c464
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 14:58:48 +0100

    # Conflicts: #	app/src/main/java/com/example/appexamen/view/UIMain.kt
    # Conflicts:
    #	app/src/main/java/com/example/appexamen/view/UIMain.kt

--- app/src/main/java/com/example/appexamen/view/UIMain.kt
+++ app/src/main/java/com/example/appexamen/view/UIMain.kt
@@ -4,12 +4,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -19,7 +27,7 @@ import com.example.appexamen.R
 import com.example.appexamen.viewmodel.ViewModel
 
 @Composable
-fun Game(viewModel: ViewModel, text:String) {
+fun Game(viewModel: ViewModel, text: String) {
 
     Box(
         modifier = Modifier
@@ -35,17 +43,38 @@ fun Game(viewModel: ViewModel, text:String) {
         Column {
             NombreJUgador(text)
         }
+        Column(modifier = Modifier
+            .padding(top = 130.dp)) {
+            Row {
+                CreateButton(Color.Red)
+                CreateButton(Color.Yellow)
+
+            }
+            Row {
+                CreateButton(Color.Cyan)
+                CreateButton(Color.DarkGray)
+            }
+            Row {
+                CreateButton(Color.Green)
+                CreateButton(Color.Blue)
+            }
+            Row {
+                StartButton(Color.Magenta)
+            }
+        }
 
     }
 }
 
 @Composable
-fun NombreJUgador(text: String){
+fun NombreJUgador(text: String) {
 
-    Column( verticalArrangement = Arrangement.Center,
+    Column(
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(top = 30.dp, start = 80.dp)) {
+            .padding(top = 30.dp, start = 90.dp)
+    ) {
 
         Text(
             text = "Jugador: $text",
@@ -54,4 +83,41 @@ fun NombreJUgador(text: String){
         )
     }
 }
+
+@Composable
+fun CreateButton(color: Color) {
+    Button(
+        onClick = {
+        },
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = color,
+        ),
+        modifier = Modifier
+            .padding(top = 60.dp, start = 20.dp)
+            .size(width = 150.dp, height = 60.dp)
+    ) {
+
+    }
+}
+
+@Composable
+fun StartButton(color: Color) {
+    Button(
+        onClick = {
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = color,
+        ),
+        modifier = Modifier
+            .padding(top = 60.dp, start = 90.dp)
+            .size(width = 200.dp, height = 100.dp)
+            .clip(CircleShape)
+    ) {
+
+    }
+}
+
+
+
 
