379faccf57fa44e384532091523cf3e94eae62c9
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 14:47:34 +0100

    creando botones colores

--- app/src/main/java/com/example/appexamen/view/UIMain.kt
+++ app/src/main/java/com/example/appexamen/view/UIMain.kt
@@ -6,10 +6,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -54,4 +61,22 @@ fun NombreJUgador(text: String){
         )
     }
 }
+
+@Composable
+fun CreateButton(color:Color){
+    Button(
+        onClick = {
+        },
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = color,
+        ),
+        modifier = Modifier
+            .padding(3.dp)
+            .size(width = 150.dp, height = 60.dp)
+    ){
+
+    }
+}
+
 
