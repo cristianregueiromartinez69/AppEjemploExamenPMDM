44bd588c7155b3931aee66de4723ea1c91fcee2b
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 11:21:44 +0100

    probando cambio de pantalla

---
+++ app/src/main/java/com/example/appexamen/view/UIMain.kt
@@ -0,0 +1,37 @@
+package com.example.appexamen.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.appexamen.R
+import com.example.appexamen.viewmodel.ViewModel
+
+@Composable
+fun Game(viewModel: ViewModel) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val backgroundImage = painterResource(id = R.drawable.fondoinicio)
+        Image(
+            painter = backgroundImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column() {
+
+        }
+    }
+}
+
